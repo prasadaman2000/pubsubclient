8eab8f0e04cce6b2ff0ed68b13f04548205a653f
Raspberry Pi OS <prasadaman2000@gmail.com>
2026-08-16 17:03:42 -0700

    more conversion of consts to flags

--- pubsubclient.go
+++ pubsubclient.go
@@ -20,9 +20,12 @@ func main() {
 	address := flag.String("address", "0.0.0.0", "--address defines the address to bind the server to.")
 	port := flag.Int("port", 8099, "--port defines the port to bind the server to.")
 
+	pubsubServerAddress := flag.String("pubsub_addr", "0.0.0.0", "--pubsub_addr defines the address the base pubsub server is running on.")
+	pubsubServerPort := flag.Int("pubsub_port", 8080, "--port defines the port the base pubsub server is running on.")
+
 	pubsubclientAddr := fmt.Sprintf("%s:%d", *address, *port)
 
-	p := NewPopulation(SERVER_IP, SERVER_PORT, *address)
+	p := NewPopulation(*pubsubServerAddress, *pubsubServerPort, *address)
 	http.HandleFunc("/clientConnect", p.ClientConnectEntry)
 	http.HandleFunc("/subscribe", p.SubscribeEntry)
 	http.HandleFunc("/publish", p.PublishEntry)
